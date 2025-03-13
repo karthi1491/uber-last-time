f8072f83bcdc6e9967ef59e38569313ee049e8c6
karthi1491 <karthikroyal51@gmail.com>
2025-03-13 23:59:40 +0530

    home screen and login screen

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Home() {
   return (
-    <div>Home</div>
-  )
+    <div className='h-screen pt-8 flex justify-between flex-col bg-cover bg-center bg-no-repeat bg-[url("https://images.unsplash.com/photo-1527603815363-e79385e0747e?q=80&w=1952&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")]'>
+      <img className='w-16 ml-9' src='https://tse2.mm.bing.net/th?id=OIP.5fy74desF0x3vRhdvgtjZwHaDF&pid=Api&P=0&h=180' alt='Uber' />
+      <div className='bg-white pb-7 py-4 px-4 rounded-lg shadow-md'> 
+        <h2 className='text-3xl font-bold'>Get Started with Uber</h2>
+        <Link to='/login' className=' flex justify-center w-full bg-black text-white py-3 rounded mt-4'>Continue</Link>
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
--- frontend/src/pages/UserLogin.jsx
+++ frontend/src/pages/UserLogin.jsx
@@ -2,8 +2,21 @@ import React from 'react';
   
   const UserLogin = () =>  {
 	return (
-	  <div>
-		userLogin
+	  <div className='p-7'>
+		<form>
+			<h3 className='text-xl mb-2'>What's your email 	</h3>
+				<input 
+				className='bg-gray-100 mb-7 rounded px-4 py-2 border w-full  placeholder: text-base'
+				required type='email'
+				 placeholder='email@example.com' 
+				 />
+             <h3>What's your password</h3>
+			 <input
+			 required 		className='bg-gray-100 mb-7 rounded px-4 py-2 border w-full text-lg placeholder: text-base' type='password' placeholder='password' />
+			 <button  		className='bg-black text-white rounded px-4 py-2  w-full text-lg'>Login</button>
+		
+		</form>
+		
 	  </div>
 	);
   }
